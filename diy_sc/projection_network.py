a1a4cf11fe30c7e2b031754263306e43b2f864d4
odunkel <olduebox@outlook.com>
2025-06-17 12:49:39 +0200

    fix: Correct path.

--- diy_sc/projection_network.py
+++ diy_sc/projection_network.py
@@ -127,7 +127,7 @@ def model(pretrained: bool = False):
     device = 'cpu'
     aggre_net = AggregationNetwork(feature_dims=[768,], projection_dim=768, device=device, feat_map_dropout=0.2)
     if pretrained:
-        ckpt_dir = 'ckpts/0300_dino_spair/best.pth'
+        ckpt_dir = './ckpts/0300_dino_spair/best.pth'
         pretrained_dict = torch.load(ckpt_dir, map_location=device)
         aggre_net.load_pretrained_weights(pretrained_dict)
     return aggre_net
