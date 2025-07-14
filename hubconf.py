19f6b25ebb1ad4e1c44a113ab80e306cad8c3ffa
odunkel <olduebox@outlook.com>
2025-07-14 21:40:01 +0200

    enhance: Add more model variants.

--- hubconf.py
+++ hubconf.py
@@ -1,4 +1,4 @@
 # Optional list of dependencies required by the package
 dependencies = ['torch']
 
-from diy_sc.projection_network import agg_dino, agg_sd_dino, agg_dino_384, agg_dino_128
+from diy_sc.projection_network import agg_dino, agg_dino_in3d, agg_dino_in3d_spair, agg_dino_384, agg_dino_128, agg_sd_dino, agg_sd_dino_in3d_spair,agg_sd_dino_in3d
